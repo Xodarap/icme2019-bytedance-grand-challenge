ad03ae6c6404048ed4d523f4ddbe18c1cd34de2c
Ben West <Xodarap@users.noreply.github.com>
2023-11-25 18:49:25 -0800

    audience

--- build_audience.py
+++ build_audience.py
@@ -8,8 +8,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import tensorflow as tf
 from multiprocessing import Array, Pool, Manager
 
-tf.logging.set_verbosity(tf.logging.INFO)
-flags = tf.flags
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+flags = tf.compat.v1.flags
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('data_dir', 'data', '数据目录')
@@ -39,13 +39,13 @@ train_file = os.path.join(data_dir, 'final_%s_train.txt' % track_name)
 test_file = os.path.join(data_dir, 'final_%s_test_no_anwser.txt' % track_name)
 names = ['uid', 'user_city', 'item_id', 'author_id', 'item_city', 'channel', 'finish', 'like', 'music_id', 'device_id', 'create_time', 'video_duration']
 
-tf.logging.info("============== start to build behavior and audience feature =================")
-tf.logging.info("============== loading data ==================")
-df_train = pd.read_csv(train_file, sep='\t', names=names)
-df_test = pd.read_csv(test_file, sep='\t', names=names)
+tf.compat.v1.logging.info("============== start to build behavior and audience feature =================")
+tf.compat.v1.logging.info("============== loading data ==================")
+df_train = pd.read_csv(train_file, sep='\t', names=names, encoding='utf-16')
+df_test = pd.read_csv(test_file, sep='\t', names=names, encoding='utf-16')
 df_all = pd.concat([df_train, df_test])
 del df_train, df_test
-tf.logging.info("============== load data successfully ==================")
+tf.compat.v1.logging.info("============== load data successfully ==================")
 
 # 统计每个视频\作者\音乐的观看人群
 def group_iter(df, by_name, target_name):
@@ -76,7 +76,7 @@ def convert_df(name, max_id, df):
 
 def build_audience(name, max_id, df):
     try:
-        tf.logging.info("============== building %s-audience feature =====================" % name)
+        tf.compat.v1.logging.info("============== building %s-audience feature =====================" % name)
         target_names = [('uid', max_uid)]
         df = convert_df(name, max_id, df)
         
@@ -84,20 +84,22 @@ def build_audience(name, max_id, df):
         save_dir = name
         for target_name, vocab_size in target_names:
             start_time = time.time()
-            tf.logging.info("============= %s-audience 开始. 时间: %f ===============" % (name, start_time))
+            tf.compat.v1.logging.info("============= %s-audience 开始. 时间: %f ===============" % (name, start_time))
 
             vocabulary = dict([[x, x] for x in range(vocab_size)])
             fit_transform(df, by_name, target_name, save_dir, vocabulary)
 
-            tf.logging.info("============= %s-audience 结束, Take %f seconds ===============" % (name, time.time()-start_time))
+            tf.compat.v1.logging.info("============= %s-audience 结束, Take %f seconds ===============" % (name, time.time()-start_time))
     except Exception as err:
         print(err)
+
+if __name__ == '__main__':        
+    pool = Pool(3)
+    name_ids = [('item', max_item_id), ('author', max_author_id), ('music', max_music_id)]
+    for name, max_id in name_ids:
+        # pool.apply_async(build_audience, args=(name, max_id, df_all))
+        pool.apply_async(build_audience, args=(name, df_all['%s_id' % name].max(), df_all))
         
-pool = Pool(3)
-name_ids = [('item', max_item_id), ('author', max_author_id), ('music', max_music_id)]
-for name, max_id in name_ids:
-    pool.apply_async(build_audience, args=(name, max_id, df_all))
-    
-pool.close()
-pool.join()
-tf.logging.info("All task done")
+    pool.close()
+    pool.join()
+    tf.compat.v1.logging.info("All task done")
