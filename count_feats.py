abd2e17f0f740534f425ada82ce59f1efa09caed
Ben West <Xodarap@users.noreply.github.com>
2023-11-25 19:03:33 -0800

    count_feats

--- count_feats.py
+++ count_feats.py
@@ -4,6 +4,7 @@ from collections import Counter
 import warnings
 import gc
 from multiprocessing import Pool
+import os
 
 warnings.filterwarnings("ignore")
 
@@ -16,8 +17,8 @@ test_path = os.path.join(data_path, 'final_track2_test_no_anwser.txt')
 # 开发时使用小数据集
 # train_data = pd.read_csv(train_path, sep='\t', header=None, names=cols, nrows=200)
 # test_data = pd.read_csv(test_path, sep='\t', header=None, names=cols, nrows=200)
-train_data = pd.read_csv(train_path, sep='\t', header=None, names=cols)
-test_data = pd.read_csv(test_path, sep='\t', header=None, names=cols)
+train_data = pd.read_csv(train_path, sep='\t', header=None, names=cols, encoding='utf-16')
+test_data = pd.read_csv(test_path, sep='\t', header=None, names=cols, encoding='utf-16')
 
 def downcast_data(D):
     for c, d in zip(D.columns, D.dtypes):
@@ -53,49 +54,48 @@ def count_fun(data, group_cols):
     data[new_feature] = data[new_feature].apply(lambda x: count[x])
     return data[[new_feature]]
 
+if __name__ == '__main__':
+    count_feats_list = []
+    count_feats_list.append(['time_day'])
 
-count_feats_list = []
-count_feats_list.append(['time_day'])
+    print('single feature count')
+    count_feats_list.extend([[c] for c in data.columns if c not in ['time', 'channel', 'like', 'finish']])
 
-print('single feature count')
-count_feats_list.extend([[c] for c in data.columns if c not in ['time', 'channel', 'like', 'finish']])
+    print('cross count')
+    users = ['uid']
+    # authors = ['user_city', 'author_id', 'item_city', 'channel', 'music_id', 'device', 'duration_time']
+    authors = ['item_id', 'user_city', 'author_id', 'item_city', 'channel', 'music_id', 'device', 'duration_time']
+    count_feats_list.extend([[u_col, a_col] for u_col in users for a_col in authors])
 
-print('cross count')
-users = ['uid']
-# authors = ['user_city', 'author_id', 'item_city', 'channel', 'music_id', 'device', 'duration_time']
-authors = ['item_id', 'user_city', 'author_id', 'item_city', 'channel', 'music_id', 'device', 'duration_time']
-count_feats_list.extend([[u_col, a_col] for u_col in users for a_col in authors])
+    users = ['author_id']
+    # authors = ['item_city', 'music_id', 'duration_time', 'time_day']
+    authors = ['channel', 'user_city', 'item_city', 'music_id', 'duration_time', 'time_day']
+    count_feats_list.extend([[u_col, a_col] for u_col in users for a_col in authors])
 
-users = ['author_id']
-# authors = ['item_city', 'music_id', 'duration_time', 'time_day']
-authors = ['channel', 'user_city', 'item_city', 'music_id', 'duration_time', 'time_day']
-count_feats_list.extend([[u_col, a_col] for u_col in users for a_col in authors])
+    count_feats_list.append(['uid', 'user_city', 'channel', 'device'])
+    count_feats_list.append(['author_id', 'item_city', 'music_id', 'duration_time'])
 
-count_feats_list.append(['uid', 'user_city', 'channel', 'device'])
-count_feats_list.append(['author_id', 'item_city', 'music_id', 'duration_time'])
+    # 根据自身配置修改 线程数
+    processor = 12
+    res_list = []
+    p = Pool(processor)
 
-# 根据自身配置修改 线程数
-processor = 12
-res_list = []
+    for i in range(len(count_feats_list)):
+        res_list.append(p.apply_async(count_fun,
+                                    args=(data[count_feats_list[i]], count_feats_list[i])))
+        print(str(i) + ' processor started !')
+    p.close()
+    p.join()
+    print('done!')
 
-p = Pool(processor)
+    res = pd.concat([res.get() for res in res_list], axis=1)
 
-for i in range(len(count_feats_list)):
-    res_list.append(p.apply_async(count_fun,
-                                  args=(data[count_feats_list[i]], count_feats_list[i])))
-    print(str(i) + ' processor started !')
-p.close()
-p.join()
-print('done!')
+    res.head()
 
-res = pd.concat([res.get() for res in res_list], axis=1)
+    # data = pd.concat([data, res], axis=1)
 
-res.head()
+    new_feats_list = ['_'.join(c) + '_count' for c in count_feats_list]
 
-# data = pd.concat([data, res], axis=1)
+    # data[new_feats_list].head()
 
-new_feats_list = ['_'.join(c) + '_count' for c in count_feats_list]
-
-# data[new_feats_list].head()
-
-res[new_feats_list].to_csv('data/track2_count_feats.csv', index=False, sep='\t')
+    res[new_feats_list].to_csv('data/track2_count_feats.csv', index=False, sep='\t')
