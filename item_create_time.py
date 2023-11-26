82163c84014fedeb8614513aaf1441ca5e26fb20
Ben West <Xodarap@users.noreply.github.com>
2023-11-25 19:36:53 -0800

    item_create_time

--- item_create_time.py
+++ item_create_time.py
@@ -6,8 +6,8 @@ import scipy.sparse
 from scipy.sparse import csr_matrix
 from sklearn.feature_extraction.text import TfidfVectorizer
 import tensorflow as tf
-tf.logging.set_verbosity(tf.logging.INFO)
-flags = tf.flags
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+flags = tf.compat.v1.flags
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('data_dir', 'data', '数据目录')
@@ -24,9 +24,9 @@ test_file = os.path.join(data_dir, 'final_%s_test_no_anwser.txt' % track_name)
 names = ['uid', 'user_city', 'item_id', 'author_id', 'item_city', 'channel', 'finish', 'like', 'music_id', 'device_id', 'create_time', 'video_duration']
 
 
-tf.logging.info("============== loading data ==================")
-df_train = pd.read_csv(train_file, sep='\t', names=names)
-df_test = pd.read_csv(test_file, sep='\t', names=names)
+tf.compat.v1.logging.info("============== loading data ==================")
+df_train = pd.read_csv(train_file, sep='\t', names=names, encoding='utf-16')
+df_test = pd.read_csv(test_file, sep='\t', names=names, encoding='utf-16')
 df_all = pd.concat([df_train, df_test])
 
 create_time = df_all[['item_id', 'create_time']].drop_duplicates('item_id')
@@ -34,4 +34,4 @@ create_time = create_time.set_index('item_id')
 
 create_time.to_csv(os.path.join(data_dir, '%s_item_create_time.csv' % track_name))
 
-tf.logging.info("============== successfully ==================")
+tf.compat.v1.logging.info("============== successfully ==================")
